b819455e9231f396197723f64169fce0c201a5e3
Red <sam.r.jones@mac.com>
2023-10-24 15:27:22 -0400

    Initial Commit - Creates Bonus Links in Header

---
+++ scripts/content.js
@@ -0,0 +1,75 @@
+console.log("foo")
+async function QuickerBase(AppDBID) {
+    
+    var curLocation = window.location.href;
+    var realm = (curLocation.split(".")[0]) + ".quickbase.com/";
+    
+    var curDbid = (curLocation.split("db/")[1]).split("?")[0];
+    
+    var appDbid = ""
+    async function addDbibFetch() {
+        const response = await fetch('https://quandarycg.quickbase.com/db/bsq5jdubk');
+        
+        var appDbid = response.url
+        appDbid = appDbid.split("/").slice(-1)[0]
+        console.log("appdbid", appDbid)
+        return appDbid
+    }
+    
+    var appDbid = await addDbibFetch()
+    
+    function addButtons() {
+    var addButt = document.getElementsByClassName(" ButtonText")
+    //console.log(addButt)
+    var bonusLinkColor = getComputedStyle(document.getElementsByClassName(" ButtonText")[0]).color;
+    
+    var bonusLinksArray = [];
+    var importUrl = [curLocation.split("=")[0] + "=ImportList&dbid=" + curDbid + "&confMsg=", "Imports"];
+    bonusLinksArray.push(importUrl)
+    var fieldsUrl = [curLocation.split("=")[0] + "=listfields", "Fields"];
+    bonusLinksArray.push(fieldsUrl)
+    var rShipUrl = [curLocation.split("=")[0] + "=Relationships", "RShips"];
+    bonusLinksArray.push(rShipUrl)
+
+
+    var appLinksArray = []
+    var relDiagramUrl = ["reldiagram", "RelDiagram"];
+    appLinksArray.push(relDiagramUrl)
+
+    var rolesUrl = ["AppRolesList", "Roles"];
+    appLinksArray.push(rolesUrl)
+
+
+    var bonusLinks = ""
+
+    function addLink (url, index) {
+        if(bonusLinks!="") {
+            bonusLinks += " <span style='text-decoration: none; color: " + bonusLinkColor + "' class='ButtonText BrandBarButton'>|</span> ";
+        }
+        bonusLinks += "<a style='text-decoration: none; color: " + bonusLinkColor + "' class='ButtonText BrandBarButton'  href='" + url[0] + "'>" + url[1] + "</a>";
+    }
+    bonusLinksArray.forEach(addLink);
+
+    function addAppLink (url, index) {
+        if(bonusLinks!="") {
+            bonusLinks += " <span style='text-decoration: none; color: " + bonusLinkColor + "' class='ButtonText BrandBarButton'>|</span> ";
+        }
+        bonusLinks += "<a style='text-decoration: none; color: " + bonusLinkColor + "' class='ButtonText BrandBarButton'  href='" + realm + "db/" + appDbid + "?a=" + url[0] + "'>" + url[1] + "</a>";
+    }  
+    appLinksArray.forEach(addAppLink);
+
+    var customText = document.getElementById("globalAddRecordButton")
+
+    console.log(customText)
+    //console.log(bonusLinks);
+
+    customText.insertAdjacentHTML("beforebegin","<span style='line-height:24px; margin-right: 5px;'>" + bonusLinks + "</span>");
+    }
+    addButtons()
+  
+}
+
+window.addEventListener('load', 
+  
+QuickerBase(window["gReqAppDBID"]), false);
+
